c8e8fd683e7716f82375ce2a4892e3ac00e7717b
pratik95-ai <pratikpurane95@gmail.com>
2025-11-14 12:21:01 +0530

    update

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    print("presnt from code build")
+    print("Hi from Harshal")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
